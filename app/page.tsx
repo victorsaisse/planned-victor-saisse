3c342d954c512f1b7d032f407867624b134af80e
Victor Saisse <saissevictor@gmail.com>
2025-02-17 15:46:52 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { redirect, useSearchParams } from "next/navigation";
 
-const handleLoginRedirect = () => {
+const useLoginRedirect = () => {
   const searchParams = useSearchParams();
   if (searchParams.get("code")) {
     redirect(`/my-memories?code=${searchParams.get("code")}`);
@@ -11,7 +11,7 @@ const handleLoginRedirect = () => {
 };
 
 export default function Plamory() {
-  handleLoginRedirect();
+  useLoginRedirect();
 
   return (
     <div
