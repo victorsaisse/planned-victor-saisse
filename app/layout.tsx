4337ceb1f6f307c311b2af8c8d302e7c86fe9904
Victor Saisse <saissevictor@gmail.com>
2025-02-17 15:52:54 -0500

    clarity script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/globals.css";
 import Navbar from "@/components/nav/navbar";
 import type { Metadata } from "next";
 import { Caveat, Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const caveat = Caveat({ subsets: ["latin"], variable: "--font-caveat" });
@@ -20,6 +21,15 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${caveat.variable} antialiased`}>
+        <Script id="microsoft-clarity">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "qbf7v2kr7w");
+          `}
+        </Script>
         <Navbar />
         {children}
       </body>
