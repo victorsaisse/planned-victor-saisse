a9272b301600e0f936a635fc25718ce21f94b726
Victor Saisse <saissevictor@gmail.com>
2025-02-17 10:38:55 -0500

    supabase auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/app/globals.scss";
 import Navbar from "@/components/nav/navbar";
+import { createClientServer } from "@/lib/supabase";
 import type { Metadata } from "next";
 import { Caveat, Inter } from "next/font/google";
 
@@ -11,11 +12,28 @@ export const metadata: Metadata = {
   description: "Your memories, your way",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClientServer();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    console.log("===>> error", error);
+  }
+
+  if (data?.user) {
+    const user = {
+      name: data.user.user_metadata.name!,
+      email: data.user.email!,
+      avatar: data.user.user_metadata.avatar_url!,
+    };
+
+    console.log("===>> user", user);
+  }
+
   return (
     <html lang="en">
       <body className={`${inter.variable} ${caveat.variable} antialiased`}>
