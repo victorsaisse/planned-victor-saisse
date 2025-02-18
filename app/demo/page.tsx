8e49079efd49bb2567afc7e309153f34c7a9aa33
Victor Saisse <saissevictor@gmail.com>
2025-02-18 09:48:31 -0500

    component

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,5 +1,5 @@
-import { MapPinIcon } from "@/components/icons";
 import ProfileBanner from "@/components/profile/banner";
+import ProfileInfo from "@/components/profile/profile-info";
 import ProfilePicture from "@/components/profile/profile-picture";
 
 export default function Demo() {
@@ -8,20 +8,7 @@ export default function Demo() {
       <ProfileBanner />
       <div className="max-w-[900px] mx-auto px-2 relative">
         <ProfilePicture />
-        <div className="pl-[200px] pt-2 flex flex-col gap-1">
-          <h1 className="text-2xl font-bold">Victor Saisse</h1>
-          <div className="flex flex-col gap-1">
-            <p className="text-gray-500 text-sm">
-              Passionate Software Engineer and Code Enthusiast 🚀
-            </p>
-            <p className="flex items-center gap-2">
-              <MapPinIcon />
-              <span className="text-gray-500 text-sm">
-                Montreal, QC, Canada
-              </span>
-            </p>
-          </div>
-        </div>
+        <ProfileInfo />
       </div>
     </>
   );
---
+++ components/profile/profile-info.tsx
@@ -0,0 +1,18 @@
+import { MapPinIcon } from "@/components/icons";
+
+export default function ProfileInfo() {
+  return (
+    <div className="pl-[200px] pt-2 flex flex-col gap-1">
+      <h1 className="text-2xl font-bold">Victor Saisse</h1>
+      <div className="flex flex-col gap-1">
+        <p className="text-gray-500 text-sm">
+          Passionate Software Engineer and Code Enthusiast 🚀
+        </p>
+        <p className="flex items-center gap-2">
+          <MapPinIcon />
+          <span className="text-gray-500 text-sm">Montreal, QC, Canada</span>
+        </p>
+      </div>
+    </div>
+  );
+}
