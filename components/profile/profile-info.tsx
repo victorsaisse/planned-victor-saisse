6e27e1f96692920c7cced0836dedd4804e9659c5
Victor Saisse <saissevictor@gmail.com>
2025-02-20 19:44:12 -0500

    truncate profile

--- components/profile/profile-info.tsx
+++ components/profile/profile-info.tsx
@@ -30,20 +30,20 @@ export default function ProfileInfo({ profile }: { profile: ProfileType }) {
         setIsDialogOpen={setIsDialogOpen}
       />
       <div className="md:pl-[200px] pt-[100px] items-center md:items-start md:pt-2 flex flex-col gap-1 ">
-        <h1 className="text-2xl font-bold flex items-center gap-2">
-          {profile.name}
+        <h1 className="text-2xl font-bold flex items-center gap-2 max-w-[90%]">
+          <span className="md:max-w-[80%] md:truncate">{profile.name}</span>
           <EditButton
             tooltipText="Edit profile info"
             onClick={() => setIsDialogOpen(true)}
           />
         </h1>
-        <div className="flex flex-col gap-1">
-          <p className="text-gray-500 text-sm max-md:text-center">
+        <div className="flex flex-col gap-1 w-full ">
+          <p className="text-gray-500 text-sm max-md:text-center md:max-w-[80%] md:truncate">
             {profile.bio}
           </p>
           <p className="flex items-center gap-1 max-md:justify-center">
             <MapPin size={16} className="text-gray-500" />
-            <span className="text-gray-500 text-sm max-md:text-center">
+            <span className="text-gray-500 text-sm max-md:text-center md:max-w-[80%] md:truncate">
               {profile.location}
             </span>
           </p>
